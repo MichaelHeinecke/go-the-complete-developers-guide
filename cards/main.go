f99cca2d09ad5ba6ee12fbf8dae4c121e61a4367
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2023-12-15 16:35:18 +0100

    Variable declaration

---
+++ cards/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Variables can be initialized outside a function
+// A value can only be assigned in a function
+var deckSize int
+
+func main() {
+	// Type can be omitted
+	var card string = "Ace of Spaces"
+	fmt.Println(card)
+
+	// Alternative way of declaring a variable
+	card2 := "Ace of Hearts"
+	fmt.Println(card2)
+
+	deckSize = 52
+	fmt.Println(deckSize)
+}
