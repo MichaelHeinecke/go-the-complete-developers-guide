443170d184867cc16fba109f4c6d3ea258f72113
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2023-12-20 15:38:17 +0100

    Add shuffle function

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Variables can be initialized outside a function
 // A value can only be assigned in a function
 
@@ -8,10 +10,17 @@ func main() {
 	cards.print()
 
 	hand, remainingCards := deal(cards, 5)
+	fmt.Println("Dealt hand:")
 	hand.print()
+	fmt.Println("Cards remaining in deck:")
 	remainingCards.print()
 
 	hand.saveToFile("my-hand")
 	handFromFile := newDeckFromFile("my-hand")
+	fmt.Println("Hand after writing it to disk and reading it from disk:")
 	handFromFile.print()
+
+	fmt.Println("Cards after shuffling:")
+	cards.shuffle()
+	cards.print()
 }
