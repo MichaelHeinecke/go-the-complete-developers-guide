c6e38c6bc41d71f28e5c9901181509717305cb7f
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2023-12-23 10:13:12 +0100

    Initialize struct without assigning field values

--- structs/main.go
+++ structs/main.go
@@ -11,4 +11,13 @@ func main() {
 	alex := person{"Alex", "Anderson"}
 	peter := person{firstName: "Peter", lastName: "Parker"}
 	fmt.Println(alex, peter)
+
+	// go assigns "zero values" to unassigned fields when initializing a struct
+	var gerry person
+	fmt.Println(gerry)
+	fmt.Printf("%+v", gerry)
+
+	gerry.firstName = "Gerry"
+	gerry.lastName = "Goobler"
+	fmt.Printf("%+v", gerry)
 }
